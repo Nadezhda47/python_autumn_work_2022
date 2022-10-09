c30a44e364a83cf7d20d4e5a1379b8274b75f291
Nadezhda47 <silen@bk.ru>
2022-10-10 01:14:01 +0300

    дз

--- unit_one/task21.py
+++ unit_one/task21.py
@@ -1,26 +1,42 @@
 # todo: Задан словарь, его значения необходимо внести по соответвющим тегам и атрибутам вместо вопросов (?)
 # заполненный шаблон записать в файл index.html
 
+key_mass = []
+k = 0 #Переменная для ключей
+i = 1 #Переменная для перебора строк
+template1 = """"""
+
 page = {"title": "Тег BODY",
         "charset": "utf-8",
         "alert": "Документ загружен",
         "p": "Ut wisis enim ad minim veniam,  suscipit lobortis nisl ut aliquip ex ea commodo consequat."}
 
-f = open("index.html", mode='w+', encoding="utf-8")
+
 template = """ 
 <!DOCTYPE HTML>
 <html>
  <head>
-  <title> """,page['title'],""" </title>
-  <meta charset=""",page['charset'],""">
+  <title> ? </title>
+  <meta charset=?>
  </head>
- <body onload="alert(""",page['alert'],""")">
+ <body onload="alert(?)">
 
-  <p>""",page['p'],"""</p>
+  <p>?</p>
  </body>
 </html>
 """
-f.writelines(template)
-wt = f.readlines()
-f.seek(39,0)
 
+
+key_mass = list(page.keys())
+print(key_mass)
+print(template.count("?"))
+template1 = template
+
+for i in range(template.count("?")):
+    template1 = template1.replace("?",key_mass[i], 1)
+    print(template1)
+    print(i)
+    i += 1
+    
+f = open("index.html", mode='a', encoding="utf-8")
+f.writelines(template1)
